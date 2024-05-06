c8292dab156bf2365b52d549ac40941ca26fbf35
claudiovasquezb <claudio.v.ballesta@gmail.com>
2024-05-06 19:59:24 -0400

    creating login screen

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent, PasswordRecoveryComponent, RegisterComponent } from './components';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'login', pathMatch: 'full'
+  },
+  { 
+    path: 'login', component: LoginComponent 
+  },
+  { 
+    path: 'register', component: RegisterComponent 
+  },
+  { 
+    path: 'password-recovery', component: PasswordRecoveryComponent 
+  },
+  { 
+    path: '**', redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-body': '#1E1E1E',
+        'dark-container': '#2C2C2C',
+        'accent': '#0C8CE9',
+        'dark-input':'#222222',
+        'dark-placeholder': 'B0B0B0',
+        'selected': '#4A5878'
+      },
+      borderRadius: {
+        DEFAULT: '0,625rem'
+      }
+    },
   },
   plugins: [],
 }
