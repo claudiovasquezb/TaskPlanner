aa867740c4a3e00763fa669e4703ab3c4574b2ec
claudiovasquezb <claudio.v.ballesta@gmail.com>
2024-05-06 11:20:12 -0400

    adding auth routing, auth pages folder and password recovery components, dashboard module with boards and highlights components.

---
+++ src/app/auth/pages/auth-screen/auth-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-screen.component.html',
+  styleUrl: './auth-screen.component.css'
+})
+export class AuthScreenComponent {
+
+}
---
+++ src/app/visitor/visitor.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VisitorRoutingModule } from './visitor-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    VisitorRoutingModule
+  ]
+})
+export class AuthModule { }
