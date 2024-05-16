d00e31e23a2e322031e76a55e65212985e8c0a57
claudiovasquezb <claudio.v.ballesta@gmail.com>
2024-05-16 16:03:42 -0400

    validating fields in createUser (auth/register)

--- app/backend/src/controllers/auth.js
+++ app/backend/src/controllers/auth.js
@@ -1,8 +1,80 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+
 
 const createUser = async(req, res) => {
 
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({
+                ok: false,
+                msg: 'A user exists with that email'
+            });
+        }
+        user = new User(req.body);
+
+        // Encrypt password
+        const saltRounds = 10;
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            bcrypt.hash(password, salt, async(err, hash) => {
+                user.password = hash;
+                // generate JWT
+
+
+                await user.save();
+            });
+        });
+
+        res.status(201).json({
+            ok: true,
+            name: user.name
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Talk to administrator'
+        });
+    }
 }
 const loginUser = async(req, res) => {
+
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if( !user ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'user does not exist'
+            });
+        }
+        // Compare password with user's encrypted password
+        bcrypt.compare(password, user.password, function(err, result) {
+            if(!result) {
+                return res.status(400). json({
+                    ok: false,
+                    msg: 'Incorrect password'
+                });
+            }
+        });
+
+        // Generate JWT
+
+
+        res.json({
+            ok: true,
+            name: user.name
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Talk to administrator'
+        });
+    }
+
 
 }
 const renewToken = async(req, res) => {
--- app/backend/src/routes/auth.js
+++ app/backend/src/routes/auth.js
@@ -1,8 +1,21 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
+const { createUser, loginUser } = require('../controllers/auth');
+const { validateFields } = require('../middlewares/validate-field');
 
 const router = Router();
 
+router.post('/register', 
+[
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Email is required').isEmail(),
+    check('password', 'Password must be at least 6 characters long').isLength({min:6}),
+    validateFields
+],
+createUser);
+
+router.post('/', loginUser);
+router.get('/renew');
 
 
 module.exports = router;
