62346fd3cdde5aee77274abf523bb34c4ffab578
claudiovasquezb <claudio.v.ballesta@gmail.com>
2024-05-27 16:07:12 -0400

    creating token interceptor

--- app/frontend/src/app/interceptors/token.interceptor.ts
+++ app/frontend/src/app/interceptors/token.interceptor.ts
@@ -1,5 +1,15 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TokenService } from '../services/token.service';
 
-export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+export const tokenInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
+  const tokenService = inject(TokenService);
+  const token = tokenService.getToken();
+  if( token ) {
+    req = req.clone({
+      headers: req.headers.set('x-token', token)
+    });
+  }
   return next(req);
 };
